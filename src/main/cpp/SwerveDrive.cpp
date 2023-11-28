df22a1186c8549387113215334ef4c1ff3574d77
sunnythecode <45048308+sunnythecode@users.noreply.github.com>
2023-11-27 18:12:49 -0800

    uncomment sd.cpp

--- src/main/cpp/SwerveDrive.cpp
+++ src/main/cpp/SwerveDrive.cpp
@@ -1,68 +1,68 @@
 
-// #include "SwerveDrive.h"
-// #include <cmath>
+#include "SwerveDrive.h"
+#include <cmath>
 
-// /*
-//  * Steps to basic swerve drive
-//  * Left stick controls robot velocity(direction + speed)
-//  * Right stick controls robot rotation
-//  *
-//  * Field Relative swerve drive: Convert desired speeds to robot relative desired speeds
-//  *
-//  *
-//  */
+/*
+ * Steps to basic swerve drive
+ * Left stick controls robot velocity(direction + speed)
+ * Right stick controls robot rotation
+ *
+ * Field Relative swerve drive: Convert desired speeds to robot relative desired speeds
+ *
+ *
+ */
 
-// void SwerveDrive::Drive(double rightX, double leftX, double leftY, double fieldRelativeGyro)
-// {
-//     /* Idea here is that controller input is a circle
-//      * Normalizing the x, y to maxSpeed will not create a hyp > maxSpeed
-//      * At pi/4 rad, the x, y both are root(2)
-//      */
+void SwerveDrive::Drive(double rightX, double leftX, double leftY, double fieldRelativeGyro)
+{
+    /* Idea here is that controller input is a circle
+     * Normalizing the x, y to maxSpeed will not create a hyp > maxSpeed
+     * At pi/4 rad, the x, y both are root(2)
+     */
 
-//     // Convert field relative desired velocities to robot relative desired velocities
-//     //  AKA fromFieldRelativeSpeeds
+    // Convert field relative desired velocities to robot relative desired velocities
+    //  AKA fromFieldRelativeSpeeds
 
-//     double Vx = leftX * maxSpeed;
-//     double Vy = leftY * maxSpeed;
-//     double omega = rightX * maxRot;
+    double Vx = leftX * maxSpeed;
+    double Vy = leftY * maxSpeed;
+    double omega = rightX * maxRot;
 
-//     ChassisSpeeds desiredSpeeds = ChassisSpeeds::fromFieldRelativeSpeeds(Vx, Vy, omega, fieldRelativeGyro);
+    ChassisSpeeds desiredSpeeds = ChassisSpeeds::fromFieldRelativeSpeeds(Vx, Vy, omega, fieldRelativeGyro);
 
-//     std::vector<SwerveModuleState> moduleStates = m_kinematics.toSwerveStates(desiredSpeeds);
+    std::vector<SwerveModuleState> moduleStates = m_kinematics.toSwerveStates(desiredSpeeds);
 
-//     // mFrontLeft.setModuleState(moduleStates[0]);
-//     // mFrontRight.setModuleState(moduleStates[1]);
-//     // mBackRight.setModuleState(moduleStates[2]);
-//     // mBackLeft.setModuleState(moduleStates[3]);
-// }
+    // mFrontLeft.setModuleState(moduleStates[0]);
+    // mFrontRight.setModuleState(moduleStates[1]);
+    // mBackRight.setModuleState(moduleStates[2]);
+    // mBackLeft.setModuleState(moduleStates[3]);
+}
 
-// void SwerveDrive::setModuleVelocity(SwerveModule &mModule, double speed, double angleRadians)
-// {
-//     mModule.setDriveVelocitySetpoint(speed);
-//     mModule.setSteerAngleSetpoint(angleRadians);
-// }
+void SwerveDrive::setModuleVelocity(SwerveModule &mModule, double speed, double angleRadians)
+{
+    mModule.setDriveVelocitySetpoint(speed);
+    mModule.setSteerAngleSetpoint(angleRadians);
+}
 
-// void SwerveDrive::initAllMotors()
-// {
-//     for (int i = 0; i < 4; i++)
-//     {
-//         mModules[i].initMotors();
-//     }
-// }
+void SwerveDrive::initAllMotors()
+{
+    for (int i = 0; i < 4; i++)
+    {
+        mModules[i].initMotors();
+    }
+}
 
-// void SwerveDrive::initThreads()
-// {
-//     for (int i = 0; i < 4; i++)
-//     {
-//         mModules[i].exitStandbyThread();
-//     }
-// }
+void SwerveDrive::initThreads()
+{
+    for (int i = 0; i < 4; i++)
+    {
+        mModules[i].exitStandbyThread();
+    }
+}
 
-// bool SwerveDrive::stopAllMotors()
-// {
-//     for (int i = 0; i < 4; i++)
-//     {
-//         mModules[i].standbyThread();
-//     }
-//     return true;
-// }
+bool SwerveDrive::stopAllMotors()
+{
+    for (int i = 0; i < 4; i++)
+    {
+        mModules[i].standbyThread();
+    }
+    return true;
+}
